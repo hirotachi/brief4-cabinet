0253139ba1d3068855db3815b73e00deb2920dfb
Said Oudouane <saidoudouane@gmail.com>
2022-01-14 00:13:18 +0100

    add home promotional section with responsive styling

--- developement/components/layout.component.php
+++ developement/components/layout.component.php
@@ -7,22 +7,21 @@ function createStyleLink($link)
 
 function layoutStart($styles = [], $title = "cabinet rafik", $with_nav_comp = true)
 {
-    echo '
+    echo "
 <!doctype html>
-<html lang="en">
+<html lang='n'
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <link
-      href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css"
-      rel="stylesheet"
-      type="text/css"
-      crossorigin="anonymous"
+      href='https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css'
+      rel='stylesheet'
+      type='text/css'      crossorigin='anonymous'
     />
     <title>$title</title>
-    ';
+    ";
     createStyleLink("styles");
     foreach ($styles as $style) {
         createStyleLink($style);
@@ -34,7 +33,17 @@ function layoutStart($styles = [], $title = "cabinet rafik", $with_nav_comp = tr
     echo "</head><body>";
 }
 
-function layoutEnd()
+
+function createScript($name)
 {
+    echo "<script src='./scripts/$name.js'></script>";
+}
+
+function layoutEnd($scripts = [])
+{
+    createScript("main");
+    foreach ($scripts as $script) {
+        createStyleLink($script);
+    }
     echo "</body></html>";
 }
