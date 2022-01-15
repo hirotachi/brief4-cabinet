78b5597a10f0c7b0247108e2e8a481604e3347dc
Said Oudouane <saidoudouane@gmail.com>
2022-01-15 22:05:07 +0100

    add patient table on tablet breakpoint with logic to handle opening and closing options

--- developement/components/dashboard/patients.component.php
+++ developement/components/dashboard/patients.component.php
@@ -1,1 +1,38 @@
-<div class="patients">patients list</div>
+<?php
+$patient = [
+    uniqid(), "Xun Guiying", "(212) 654-5678", "example@gmail.com", date("m/d/y"), "covid 19"
+];
+?>
+
+
+<div class="patients">
+    <div class="columns">
+        <span></span>
+        <span>name</span>
+        <span>phone</span>
+        <span>email</span>
+        <span>birthdate</span>
+        <span>sickness</span>
+        <span>more</span>
+    </div>
+    <?php
+    for ($i = 0; $i < 5; $i++) {
+        [$id, $name, $phone, $email, $date, $sickness] = $patient;
+        echo "<div class='patient'>
+        <img src='assets/images/avatars/400.jpg' alt='avatar'/>
+        <span>$name</span>
+        <span>$phone</span>
+        <span>$email</span>
+        <span>$date</span>
+        <span>$sickness</span>
+        <div class='more'>
+        <span class='more_btn'><i class='far fa-ellipsis-h'></i></span>
+            <div class='more_options'>
+                <span class='option--danger'>remove</span>
+                <span>edit</span>    
+            </div>
+        </div>
+    </div>";
+    }
+    ?>
+</div>
--- developement/public/scripts/dashboard.js
+++ developement/public/scripts/dashboard.js
@@ -8,3 +8,32 @@ function handleFocus() {
 
 filterSearchInput.addEventListener("focus", handleFocus)
 filterSearchInput.addEventListener("blur", handleFocus)
+
+
+const patientMoreBtns = document.querySelectorAll(".patient .more")
+const patientMoreOpenClass = "more_options--open";
+patientMoreBtns.forEach((btn, index) => {
+    const optionsEl = btn.querySelector(".more_options");
+
+    onClickOutside(btn, () => {
+        const isOpen = optionsEl.classList.contains(patientMoreOpenClass);
+        if (isOpen) optionsEl.classList.remove(patientMoreOpenClass);
+    })
+    btn.addEventListener("click", (e) => {
+        optionsEl.classList.toggle(patientMoreOpenClass);
+    })
+})
+
+
+/**
+ * execute callback when click event is clicked outside it
+ * @param el
+ * @param cb
+ */
+function onClickOutside(el, cb) {
+    document.addEventListener("click", (e) => {
+        const target = e.target;
+        // check if clicking inside the element
+        if (!el.contains(target)) cb(target);
+    })
+}
