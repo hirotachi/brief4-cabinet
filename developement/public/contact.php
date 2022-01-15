1b05eaf355362abe85b69d07c0a854aefe981281
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-01-15 23:22:02 +0100

    Add login and contact pages

--- developement/public/contact.php
+++ developement/public/contact.php
@@ -8,9 +8,7 @@ nav(); ?>
 <div class="contact">
     <!-- les informations -->
     <div class ="information">
-        <h6>Contact Us</h6>
-        <h1>How can I help you?</h1>
-        <h5>Fill in the form or drop an email</h5>
+        <h4>Contact Us</h4>
         <div class="help">
             <img src='assets/phone-contact.svg' alt='icon'>
             <span>+212600000000</span>
@@ -26,10 +24,14 @@ nav(); ?>
     </div>
     <form>
         <div class="fill">
+            <div class="title">
+                <h1>How can I help you?</h1>
+                <h5>Fill in the form or drop an email</h5>
+            </div>
             <input class="input" type="name" placeholder="Name">
             <input class="input" type="email" placeholder="Email">
             <input class="input" type="Subject" placeholder="Subject">
-            <textarea class="input" type="Message" placeholder="Message"></textarea>
+            <textarea class="textarea" type="Message" placeholder="Message"></textarea>
             <input type="Submit" value="Submit">
         </div>
     </form>
