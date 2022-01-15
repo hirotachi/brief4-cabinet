a6de2df5386d125eeccf86392e60cb1b7762a3ba
Said Oudouane <saidoudouane@gmail.com>
2022-01-15 18:28:58 +0100

    remove redundant sass code and rewrite gulp config to handle php files and reload the page

--- developement/gulpfile.js
+++ developement/gulpfile.js
@@ -3,6 +3,35 @@ const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
 const sass = require("gulp-sass")(require("sass"));
 
+const browserSync = require("browser-sync");
+const phpConnect = require('gulp-connect-php');
+
+//Php connect
+function connectSync() {
+    phpConnect.server({
+        port: 8000,
+        keepalive: true,
+        base: "public"
+    }, function () {
+        browserSync({
+            proxy: '127.0.0.1:8000',
+        });
+    });
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+
+// Watch files
+function watchPhp() {
+    gulp.watch("./**/*.php", gulp.series(browserSyncReload));
+}
+
+
 function buildStyles() {
     cleanStyles();
     return gulp
@@ -20,9 +49,9 @@ function cleanStyles() {
     return del(["./public/styles/**/*.css"])
 }
 
-exports.buildStyles = buildStyles;
+function watchSass() {
+    gulp.watch("./src/styles/**/*.scss", gulp.series(buildStyles, browserSyncReload))
+}
 
-exports.watch = function () {
-    buildStyles();
-    gulp.watch("./src/styles/**/*.scss", buildStyles);
-};
+exports.watch = gulp.parallel([watchPhp, watchSass, connectSync]);
+
